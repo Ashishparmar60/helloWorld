e72ed04bc1f7511d5c251617e35775bcad97a98a
Ashishparmar60 <ashishparmar60@gmail.com>
2022-08-21 19:40:14 +0530

    Changed back to hello world

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main() {
-int num1;
-int num2;
-printf("Enter first number: ");
-scanf("%d", num1);
-printf("Enter Second number: ");
-scanf("%d", num2);
-
-
-printf("Answer: %d", num1 + num2 );
-  return 0;
-  }
+printf("Hello World");
+}
